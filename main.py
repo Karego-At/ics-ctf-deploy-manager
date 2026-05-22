2b02302415d8f32e2c15db927d9b206463899c46
Your Name <you@example.com>
2026-05-22 20:29:13 +0200

    add test network and peer logic

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from src.test import run_pn_dev
 import os
 from src.config import get_config
+from ipaddress import IPv4Address
+
+
+
 
 path = os.getcwd()
 
@@ -18,13 +22,29 @@ conf = get_config(conf_path)
 print(conf)
 
 a = subfolders = [ f for f in os.scandir(components_path) if f.is_dir() and f.name == "pnet-dev"]
+b = subfolders = [ f for f in os.scandir(components_path) if f.is_dir() and f.name == "attacker-ssh"]
+
 print(type(a[0]))
+
+
+nic = conf.devices[0].nic
+print(type(nic))
+
 
 
 from docker.models.images import Image
 import docker
 
 client = docker.from_env()
+
+
+
+
+
+
+from docker.models.networks import Network as DockerNetwork
+from docker.models.containers import Container
+from src.config import PLC
 
 
 class Component():
@@ -39,9 +59,55 @@ class Component():
 
 
 
-from docker.models.networks import Network as DockerNetwork
-from docker.models.containers import Container
-from src.config import PLC
+# class DockerPeer():
+#     name: str
+#     container: Container
+#     network: DockerNetwork
+
+#     def __init__(self, container:Container, network:DockerNetwork, external_network = False):
+#         self.container = container
+#         self.network = network
+#         if external_network :
+
+
+        
+class ConnectablePeer():
+
+    name: str
+    container: Container
+    networks: list[DockerNetwork] = []
+    externalNetwork: DockerNetwork
+
+
+    def __init__(self, name: str, image: Image):#, ip: IPv4Address):
+
+        self.externalNetwork = client.networks.create(name + "_external_network", driver="bridge")
+
+        # networking_config=client.api.create_networking_config({
+        # "my_network": client.api.create_endpoint_config(
+        #     ipv4_address=ip
+        # )
+        # })
+
+
+        container = client.containers.run(
+        image=image.id, 
+        name=name,
+        network=self.externalNetwork.name,
+        detach=True,
+        ports={'22': 2020},  
+        # networking_config = networking_config,  
+        remove= True
+        )
+        self.container = container
+            
+    def destroy(self):
+        self.container.kill()
+        self.container.remove()
+        self.externalNetwork.remove()
+
+
+
 
 class MCVNetwork:
     network:    DockerNetwork
@@ -53,27 +119,46 @@ class MCVNetwork:
         self.hw_device = device
         self.name = name
         ipam_pool = docker.types.IPAMPool(
-        subnet= device.nw
-        # gateway='192.168.101.1',
-        # iprange='192.168.101.192/28'
+        subnet=  str(device.nw),
+        gateway='192.168.101.100',
+        iprange='192.168.101.0/24'
         ) 
         ipam_config = docker.types.IPAMConfig(
             driver='default',
             pool_configs=[ipam_pool]
         )
-        self.network = client.networks.create(name, driver="macvlan", options={"parent": device.nic})#, ipam=ipam_config)
+        self.network = client.networks.create(name, driver="macvlan", options={"parent": device.nic}, ipam=ipam_config)
     
 
-    def add_device(self, name:str, comp:Component):
+    def add_device(self, name:str, image:Image, ip:IPv4Address):
+
+
+        networking_config=client.api.create_networking_config({
+        "my_network": client.api.create_endpoint_config(
+            ipv4_address=ip
+        )
+        })
+
+
+
         container = client.containers.run(
-            image=comp.image.id, 
-           # name=name,
+            image=image.id, 
+            name=name,
             network=self.network.name,
-            detach=True
+            detach=True,
+            networking_config = networking_config,  
+            remove= True
+             
         )
         self.docker_devices.append(container)
         return container
-    
+
+    def connect(self, peer:ConnectablePeer):
+        peer.networks.append(self.network)
+        # self.docker_devices.append(peer.container)
+        self.network.connect(peer.container)
+
+
     def destruct(self):
         print("self destruction")
         for cont in self.docker_devices:
@@ -95,15 +180,28 @@ class MCVNetwork:
 
 comp1 = Component(a[0].path)
 
+
 nw = MCVNetwork("nw3", conf.devices[0])
 
-nw.add_device(name="hw-sens-man", comp=comp1)
+nw.add_device(name="hw-sens-man", image=comp1.image, ip="192.168.101.1")
+
+
+
+print(conf.devices[0].nw)
 
 
 
 
 
+comp2 = Component(b[0].path)
+
+attacker = ConnectablePeer("attacker", comp2.image)
+
+nw.connect(attacker)
 
 
+input()
 
-# nw.destruct()
+nw.destruct()
+
+attacker.destroy()
